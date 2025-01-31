38c35c9ec9cbf5ee7d3534d2edbc8fd2ec3446b3
Pavlo <pavdemchnko@gmail.com>
2025-01-30 21:01:23 -0500

    Added 2 and 3 tasks

---
+++ eduHillel_HW10.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace eduHillel_HW10
+{
+    public static class Program
+
+    {
+        static void Main(string[] args)
+        {
+            string textInput;
+            int eventCount = 1;
+
+            StringBuilder textReport = new StringBuilder();
+            textReport.AppendLine("Text report");
+            textReport.AppendLine($"{DateTime.Now}");
+            textReport.AppendLine("List of Events: ");           
+
+            while (true)
+            {
+                Console.WriteLine("Insert event. Insert 'finish' or click Enter to complete:");
+                textInput = Console.ReadLine();
+
+                if (textInput.ToLower() == "finish" || string.IsNullOrWhiteSpace(textInput))
+                    break;
+                textReport.AppendLine($"{eventCount}. {textInput};");
+                eventCount++;
+            }
+            Console.WriteLine(textReport.ToString());
+        }
+    }
+}
---
+++ eduHillel_HW10.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace eduHillel_HW10
+{
+    public static class Program
+
+    {
+        static void Main(string[] args)
+        {
+            string textInput;
+            string result;
+
+            Console.WriteLine("Input text with spacebars:");
+            textInput = Console.ReadLine();
+            result = textInput.Replace(" ", "");
+            Console.WriteLine("Result without spacebars:");
+            Console.WriteLine(result);
+        }
+    }
+}
